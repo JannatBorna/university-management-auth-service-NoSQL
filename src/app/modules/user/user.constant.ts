28429f364596f92e981f8adfca5ff47aef0e8bfe
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2023-09-19 21:43:14 +0600

    add

---
+++ src/app/modules/user/user.constant.ts
@@ -0,0 +1,3 @@
+export const EVENT_STUDENT_CREATED = 'student.created';
+// export const EVENT_ACADEMIC_DEPARTMENT_UPDATED = 'academic-department.updated';
+// export const EVENT_ACADEMIC_DEPARTMENT_DELETED = 'academic-department.deleted';
